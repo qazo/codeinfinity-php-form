3df38cfbbe386c6abcddedaa3dedd9e7cbc571c3
Kwezilomso Mhaga <kwezimhaga@live.com>
2021-09-21 17:36:32 +0200

    check length on nme and surname

--- src/Validation/UserValidator.php
+++ src/Validation/UserValidator.php
@@ -16,6 +16,15 @@ class UserValidator
 	{
 		$this->errors = [];
 		$dateOfBirth = $this->user->getDateOfBirth();
+
+		if (strlen($this->user->getName()) == 0) {
+			$this->errors[] = "Please enter a name.";
+		}
+
+		if (strlen($this->user->getSurname()) == 0) {
+			$this->errors[] = "Please enter a surname.";
+		}
+
 		if (!DateOfBirthValidator::isValidDateOfBirth($dateOfBirth)) {
 			$this->errors[] = "Date of Birth is invalid. Date of birth must be in the format of 'dd/mm/YYYY' (31-12-2020).";
 		}
